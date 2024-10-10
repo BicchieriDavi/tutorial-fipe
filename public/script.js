3a58a9518e2209f37b042e6d6d0a9095234b21d4
BicchieriDavi <bicchieridavi.dp@gmail.com>
2024-10-09 22:27:33 -0300

    Minha solução depois do aprendizado

--- public/script.js
+++ public/script.js
@@ -22,3 +22,79 @@
 
     resultado.style.display = 'block';
 */
+const tipo_veiculo = document.querySelector("#tipo-veiculo");
+const marca = document.querySelector("#marca");
+const modelo = document.querySelector("#modelo");
+const versao = document.querySelector("#versao");
+const formulario = document.querySelector("#consulta-form");
+
+tipo_veiculo.value = null;
+
+tipo_veiculo.addEventListener("change", function () {
+  const valor = tipo_veiculo.value;
+
+  marca.innerHTML = '<option value="">Selecione a Marca</option>';
+  modelo.innerHTML = '<option value="">Selecione o Modelo</option>';
+  versao.innerHTML = '<option value="">Selecione a Versão</option>';
+
+  axios.get(`fipe/${valor}`).then((resposta) => {
+    const data = resposta.data;
+
+    data
+      .map((elemento) => {
+        marca.innerHTML += `<option value="${elemento.code}">${elemento.name}</option>`;
+      })
+      .join("");
+  });
+});
+
+marca.addEventListener("change", function () {
+  const valor = marca.value;
+
+  modelo.innerHTML = '<option value="">Selecione o Modelo</option>';
+  versao.innerHTML = '<option value="">Selecione a Versão</option>';
+
+  axios.get(`fipe/${tipo_veiculo.value}/${valor}`).then((resposta) => {
+    const data = resposta.data;
+
+    data
+      .map((elemento) => {
+        modelo.innerHTML += `<option value="${elemento.code}">${elemento.name}</option>`;
+      })
+      .join("");
+  });
+});
+
+modelo.addEventListener("change", function () {
+  const valor = modelo.value;
+
+  versao.innerHTML = '<option value="">Selecione a Versão</option>';
+
+  axios
+    .get(`fipe/${tipo_veiculo.value}/${marca.value}/${valor}`)
+    .then((resposta) => {
+      const data = resposta.data;
+
+      data
+        .map((elemento) => {
+          versao.innerHTML += `<option value="${elemento.code}">${elemento.name}</option>`;
+        })
+        .join("");
+    });
+});
+
+formulario.addEventListener("submit", function (evento) {
+  evento.preventDefault();
+  const valor = versao.value;
+  resultado.style.display = "block";
+  resultado.style.textAlign = "center";
+  axios
+    .get(`fipe/${tipo_veiculo.value}/${marca.value}/${modelo.value}/${valor}`)
+    .then((resposta) => {
+      const data = resposta.data;
+
+      resultado.innerHTML = `${data.price}`;
+    });
+
+  return false;
+});
